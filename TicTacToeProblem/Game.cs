90413c3169804a87d4301a1d54024745991a081a
[Sona] <[sona16061999@gmail.com]>
2021-07-08 11:50:57 +0530

    TicTacToe Board is created

---
+++ TicTacToeProblem/Game.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TicTacToeProblem
+{
+    class Game
+    {
+
+     public void Board()
+        {
+            char[] CreateBoard = {'0','0','0', '0','0','0','0','0','0','0' };
+            Console.WriteLine("     |     |      ");
+            Console.WriteLine("_____|_____|_____ ");
+            Console.WriteLine("     |     |      ");
+            Console.WriteLine("_____|_____|_____ ");
+            Console.WriteLine("     |     |      ");
+            Console.WriteLine("     |     |      ");
+        }
+    }
+}
---
+++ TicTacToeProblem/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace TicTacToeProblem
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Game obj = new Game();
+            obj.Board();
+        }
+    }
+}
